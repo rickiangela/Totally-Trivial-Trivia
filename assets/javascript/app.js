1a7ecbaff93019e814e8d07b87a6a1fe04922977
rickiangela <rickiangela@gmail.com>
2018-12-20 17:29:59 -0500

    Finished project

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,153 @@
-/*Random trivia
-1. Normal adult dogs have how many teeth?
+var triviaQuestions = [{
+	question: "Normal adult dogs have how many teeth?",
+	answerList: ["24", "42", "38", "32"],
+	answer: 1
+},{
+	question: "Through what part of the body do dogs sweat?",
+	answerList: ["Paws", "Nose", "Ears", "Mouth"],
+	answer: 0
+},{
+	question: "What is a dog’s most highly developed sense?",
+	answerList: ["Touch", "Sight", "Taste", "Smell"],
+	answer: 3
+},{
+	question: "What is the favorite dog breed of the Queen of England?",
+	answerList: ["Basenji", "Poodle", "Corgi", "Pomeranian"],
+	answer: 2
+},{
+	question: "Which TV series had a dog named K9 who was also a robot?",
+	answerList: ["Full House", "Star Trek", "Law & Order", "Doctor Who"],
+	answer: 3
+},{
+	question: " What is the most popular breed of dog, according to the American Kennel Club’s registrations?",
+	answerList: ["Labrador", "Golden Retriever", "Beagle", "German Shepherd"],
+	answer: 0
+},{
+	question: "How old was the world’s oldest dog, an Australian cattle hound named Bluey, in human years?",
+	answerList: ["32", "29", "27", "30"],
+	answer: 1
+},{
+	question: "What was the most popular dog name of 2011, according to VPI Pet Insurance?",
+	answerList: ["Lucy", "Max", "Bella", "Bailey"],
+	answer: 2
+}];
 
-A) 24
-B) 38
-C) 42*
-D) 32
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "BORK! You got it right!!!",
+	incorrect: "GRRR! That's not right.",
+	endTime: "*sneeze* No more time",
+	finished: "How well did you know your grrborkers?"
+}
 
-2. Through what part of the body do dogs sweat?
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
 
-A) Mouth
-B) Ears
-C) Nose
-D) Paws*
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
 
-3. What is a dog’s most highly developed sense?
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
 
-A) Taste
-B) Smell*
-C) Sight
-D) Touch
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
+	
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
 
-4. What is the favorite dog breed of the Queen of England?
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h2>Time Left: ' + seconds + '</h2>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
 
-A) Corgi*
-B) Basenji
-C) Poodle
-D) Pomeranian
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h2>Time Left: ' + seconds + '</h2>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
 
-5. Which TV series had a dog named K9 who was also a robot?
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
 
-A) Full House
-B) Star Trek
-C) Doctor Who*
-D) Law & Order
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	$('#gif').html('<img src = "assets/images/'+ gifArray[currentQuestion] +'.gif" width = "400px">');
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The right answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('OUT OF TIME!! The right answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 3000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 3000);
+	}	
+}
 
-6. What is the most popular breed of dog, according to the American Kennel Club’s registrations?
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
 
-A) Golden retriever
-B) Beagle
-C) German Shepherd
-D) Labrador*
-
-7. How old was the world’s oldest dog, an Australian cattle hound named Bluey, in human years?
-
-A) 32
-B) 27
-C) 30
-D) 29*
-
-8. What was the most popular dog name of 2011, according to VPI Pet Insurance?
-
-A) Lucy
-B) Bailey
-C) Bella*
-D) Max
-
-*/
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
