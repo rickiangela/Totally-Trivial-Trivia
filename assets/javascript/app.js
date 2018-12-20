8cc1fa54ac70377f061abed6d06b95fc39780a5e
rickiangela <rickiangela@gmail.com>
2018-12-20 10:17:54 -0500

    These are named correctly

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+/*Random trivia
+1. Normal adult dogs have how many teeth?
+
+A) 24
+B) 38
+C) 42*
+D) 32
+
+2. Through what part of the body do dogs sweat?
+
+A) Mouth
+B) Ears
+C) Nose
+D) Paws*
+
+3. What is a dog’s most highly developed sense?
+
+A) Taste
+B) Smell*
+C) Sight
+D) Touch
+
+4. What is the favorite dog breed of the Queen of England?
+
+A) Corgi*
+B) Basenji
+C) Poodle
+D) Pomeranian
+
+5. Which TV series had a dog named K9 who was also a robot?
+
+A) Full House
+B) Star Trek
+C) Doctor Who*
+D) Law & Order
+
+6. What is the most popular breed of dog, according to the American Kennel Club’s registrations?
+
+A) Golden retriever
+B) Beagle
+C) German Shepherd
+D) Labrador*
+
+7. How old was the world’s oldest dog, an Australian cattle hound named Bluey, in human years?
+
+A) 32
+B) 27
+C) 30
+D) 29*
+
+8. What was the most popular dog name of 2011, according to VPI Pet Insurance?
+
+A) Lucy
+B) Bailey
+C) Bella*
+D) Max
+
+*/
